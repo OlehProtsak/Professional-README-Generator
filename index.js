84aea3bb6c6c9a69d2c10bc74129aac28b2ebc1a
Oleh Protsak <protsak19@gmail.com>
2024-01-13 15:55:29 +0000

    created NOT finale version of markdown for generateMarkdown function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
+const questions = require("./utils/questions");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // function to write README file
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,34 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} License - see the [LICENSE.md](LICENSE.md) file for details.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For questions or concerns related to this project, please contact ${data.username} at ${data.email}.
 `;
 }
 
